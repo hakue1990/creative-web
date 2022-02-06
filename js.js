e3bd82bb7932c4f408543d97b2fac520495ac9fd
Adam <adam.haldas@gmail.com>
2022-02-06 18:13:03 +0100

    fix nav animation

--- js.js
+++ js.js
@@ -1,10 +1,18 @@
 const btn = document.querySelector('.burger');
 const list =  document.querySelector('.navbar--list');
+const listItems = document.querySelectorAll('.navbar--list li > a');
 
 btn.addEventListener('click', () => {
     list.classList.toggle('active')
     btn.classList.toggle('active')
+    listItems.forEach((item, index) => {
+        if(item.style.animation == ''){
+            item.style.animation = `show 1.5s ease ${index / 7+ .1}s `;
+        }else {
+            item.style.animation = ''
+        }
+    })
 })
 
-const listItems = document.querySelectorAll('.navbar--list li');
 
+console.log(listItems)
