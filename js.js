b806ae7388d84628be48ac559ae370bb93275bec
Adam <adam.haldas@gmail.com>
2022-02-16 20:52:59 +0100

    add scroll to top btn

--- js.js
+++ js.js
@@ -1,18 +1,36 @@
 const btn = document.querySelector('.burger');
-const list =  document.querySelector('.navbar--list');
+const list = document.querySelector('.navbar--list');
 const listItems = document.querySelectorAll('.navbar--list li > a');
-
+const scrollToTopBtn = document.getElementById('scrollToTopBtn');
+var rootElement = document.documentElement;
 btn.addEventListener('click', () => {
-    list.classList.toggle('active')
-    btn.classList.toggle('active')
-    listItems.forEach((item, index) => {
-        if(item.style.animation == ''){
-            item.style.animation = `show 1.5s ease ${index / 7+ .1}s `;
-        }else {
-            item.style.animation = ''
-        }
-    })
-})
+  list.classList.toggle('active');
+  btn.classList.toggle('active');
+  listItems.forEach((item, index) => {
+    if (item.style.animation == '') {
+      item.style.animation = `show 1.5s ease ${index / 7 + 0.1}s `;
+    } else {
+      item.style.animation = '';
+    }
+  });
+});
 
+scrollToTopBtn.addEventListener('click', scrollToTop);
+function scrollToTop() {
+  // Scroll to top logic
+  rootElement.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+}
 
-console.log(listItems)
+function handleScroll() {
+  const scrollTotal = rootElement.scrollHeight - rootElement.clientHeight;
+
+  if (rootElement.scrollTop / scrollTotal < 0.4) {
+    scrollToTopBtn.classList.add('hide');
+  } else {
+    scrollToTopBtn.classList.remove('hide');
+  }
+}
+document.addEventListener('scroll', handleScroll);
